e3496d8f12e7a84d5b7257189fe346225818b3d1
Zababurin Sergey <s.zababurin.v@gmail.com>
2020-07-02 03:40:30 +0300

    update

---
+++ server.interface.localhost.mjs
@@ -0,0 +1,3 @@
+import main from './index.interface.localhost.mjs'
+
+main.listen(6040, function () { console.log('listening on http://localhost:6040') });
--- server.mjs
+++
@@ -1,3 +0,0 @@
-import app from './index.mjs'
-
-app.listen(6040, function () { console.log('listening on http://localhost:6040') });
